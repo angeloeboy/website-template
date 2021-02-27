a61fcc38efc9f650c61a28f9976bbd0df914af6c
Angelo Zuniga <angeloeboy10@gmail.com>
2021-02-27 13:38:35 +0800

    with styles

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ $(".work").scroll(() => {
     console.log("scrolled")
 })
 
-$(".work-titles > p").click(() => {
-    console.log(this).css("color", "pink");
-})
+// $(".work-titles > p").click(() => {
+//     let className = $(this).attr("class")
+
+//     console.log(className)
+// })
+
+$(".work-titles > p").click(function() {
+    let className = this.className;
+    
+
+    $(".work-titles > p").removeClass("clicked")
+    
+    $(`.${className}`).addClass("clicked")
+ });
